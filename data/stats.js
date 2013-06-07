c56b2e1705a10136e7483a8e61ee6e1a1dfb2c9b
Abhishek Choudhary <choudhary.abhishek12@gmail.com>
2013-06-08 00:10:32 +0530

    Trying for new about page

---
+++ data/stats.js
@@ -0,0 +1,11 @@
+const{classes: Cc, interfaces: Ci, utils: Cu} = Components;
+Cu.import("resource://gre/modules/Services.jsm");
+Cu.import("resource://gre/modules/XPCOMUtils.jsm");
+
+function loader() {
+
+}
+
+function unloader() {
+
+}
