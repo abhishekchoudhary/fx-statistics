199fc98f17d564a306196e490083f1c7e61220fd
Abhishek Choudhary <choudhary.abhishek12@gmail.com>
2013-07-04 01:56:33 +0530

    Restructuring for SDK

--- data/stats.js
+++ data/stats.js
@@ -1,10 +1,9 @@
-function populateTemplate(statsBlocks, styleCss, statsFullName, statsTagLine) {
-  if (!document.getElementById("stats-h1"))
-    return;
+function populate(styleCss, statsFullName, statsTagLine) {
+  //if (!document.getElementById("stats-h1"))
+    //return;
 
   document.title = unescape(statsFullName);
   document.getElementById("stats-h1").textContext = unescape(statsFullName);
   document.getElementById("stats-h2").textContext = unescape(statsTagLine);
-  document.getElementById("stats-blocks").innerHTML = unescape(statsBlocks);
-  document.getElementById("stats-stylesheet").href = unescape(styleCss);
+  document.getElementById("stat-stylesheet").href = unescape(styleCss);
 }
