64153dbfc99b6f22ab1ca752db81e2e60f15b166
Abhishek Choudhary <choudhary.abhishek12@gmail.com>
2013-07-05 01:41:57 +0530

    A file structure that works

--- data/stats.js
+++ data/stats.js
@@ -4,6 +4,6 @@ function populate(styleCss, statsFullName, statsTagLine) {
 
   document.title = unescape(statsFullName);
   document.getElementById("stats-h1").textContext = unescape(statsFullName);
-  document.getElementById("stats-h2").textContext = unescape(statsTagLine);
+  //document.getElementById("stats-h2").textContext = unescape(statsTagLine);
   document.getElementById("stat-stylesheet").href = unescape(styleCss);
 }
